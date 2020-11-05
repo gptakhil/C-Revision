e31be3c2160049865f723d2d23fbdd6fb6af790c
gptakhil <cse.akhil002@gmail.com>
2020-11-05 14:39:08 +0530

    Add programs till Inheritance

--- temp2.cpp
+++ temp2.cpp
@@ -1,8 +1,26 @@
-#include <iostream>
-
+/*#include <iostream>
 using namespace std;
 
-int main() {
-cout<<(10+90/30-30/15*4-3);
-return 0;
+int main()
+{
+  int arr[] = { 5, 10, 8, 4 };
+  int* p = (arr + 1);
+  cout << *p + 2 <<endl;
+  cout << *arr + 10;
+  return 0;
+}*/
+
+#include <iostream>
+using namespace std;
+void cube(int *a){
+  if(a != NULL){
+    *a = (*a) * (*a) * (*a);
+  }
 }
+
+int main() {
+  int *p = new int(2);
+  cube(p);
+  cout << *p;
+}
+
